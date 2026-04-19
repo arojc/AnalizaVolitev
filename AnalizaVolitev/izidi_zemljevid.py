8026d1a61ec7eb3241e2fbb4e45a58cb32bb03d7
antonrojc <aantonrojc@gmail.com>
2026-04-19 16:53:49 +0200

    Added Python project

---
+++ AnalizaVolitev/izidi_zemljevid.py
@@ -0,0 +1,166 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# =========================
+# 1. NALOŽI PODATKE
+# =========================
+
+def draw_units(ax):
+
+    # narisi meje volilnih enot (čez)
+    enote = gpd.read_file("volilne_enote.json")
+    enote = enote.to_crs(epsg=4326)
+    enote.boundary.plot(
+        ax=ax,
+        linewidth=1,
+        edgecolor="black"
+    )
+
+def draw_districts(ax):
+
+    # narisi meje volilnih enot (čez)
+    okraji = gpd.read_file("volilni_okraji_mb_lj_po_1.json")
+    okraji = okraji.to_crs(epsg=4326)
+    okraji.boundary.plot(
+        ax=ax,
+        linewidth=0.2,
+        edgecolor="black"
+    )
+
+def posrafiraj(ax, stranka="Svoboda"):
+
+    okraji = gpd.read_file("volilni_okraji_mb_lj_po_1.json")
+    mandati = pd.read_excel("mandati.ods", sheet_name=stranka)
+
+    df = pd.read_excel("izidi26.ods", engine="odf")
+
+    okraji_id = "NAZIV"
+    rez_id = "Volilni Okraj"
+
+    gdf = okraji.merge(df, left_on=okraji_id, right_on=rez_id)
+    gdf = gdf.merge(mandati, left_on=okraji_id, right_on="Okraj")
+
+    gdf = gdf.to_crs(epsg=4326)
+
+    # fig, ax = plt.subplots(figsize=(10, 10))
+
+    gdf.plot(
+        ax=ax,
+        linewidth=1,
+        edgecolor="black",
+        hatch="...",
+        facecolor="none"
+    )
+
+    # ax.set_title("Volilni rezultati po okrajih (%)")
+    # ax.axis("off")
+
+    # plt.show()
+
+def draw_map(podatek_za_prikaz="Odstotek udeležbe", trim_range=False, posrafiram=False):
+
+    # GeoJSON (pričakujemo D96/TM)
+    okraji = gpd.read_file("volilni_okraji_mb_lj_po_1.json")
+
+    # Rezultati (ODS)
+    df = pd.read_excel("izidi26.ods", engine="odf")
+
+    # =========================
+    # 2. PREVERI IMENA STOLPCEV
+    # =========================
+    print("OKRAJI columns:", okraji.columns)
+    print("ODS columns:", df.columns)
+
+    # >>> TU PRILAGODI IMENA <<<
+    okraji_id = "NAZIV"
+    rez_id = "Volilni Okraj"
+    value_col = podatek_za_prikaz   # npr. % glasov
+
+    gdf = okraji.merge(df, left_on=okraji_id, right_on=rez_id)
+
+    print(gdf[[okraji_id, value_col]].head())
+    print(gdf[value_col].describe())
+
+    gdf = gdf.to_crs(epsg=4326)
+
+    if trim_range:
+        vmin=gdf[value_col].min()
+        vmax=gdf[value_col].max()
+    else:
+        vmin=0
+        vmax=1
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # narisi okraje (barvani)
+    gdf.plot(
+        column=value_col,
+        cmap="viridis",
+        linewidth=0.2,
+        edgecolor="white",
+        legend=True,
+        vmin=vmin,
+        vmax=vmax,
+        ax=ax
+    )
+
+    if(posrafiram):
+        posrafiraj(ax, podatek_za_prikaz.split('-')[0])
+
+    draw_units(ax)
+
+    ax.set_title("Volilni rezultati po okrajih (%)")
+    ax.axis("off")
+
+    plt.show()
+
+def draw_dots():
+
+    # GeoJSON s točkami
+    points = gpd.read_file("tocke.geojson")
+    mandati = pd.read_excel("mandati.ods", sheet_name="SDS")
+
+    # ODS rezultati
+    df = pd.read_excel("izidi26.ods", engine="odf")
+
+    # # >>> PRILAGODI <<<
+    points_id = "name"  # ID v GeoJSON
+    rez_id = "Volilni Okraj"  # ID v ODS
+    value_col = "Odstotek udeležbe"  # vrednost (0–100)
+    gdf = points.merge(df, left_on=points_id, right_on=rez_id)
+    gdf = gdf.merge(mandati, left_on=points_id, right_on="Okraj")
+
+    gdf = gdf.to_crs(epsg=4326)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    gdf.plot(
+        column=value_col,
+        cmap="viridis",
+        markersize=70,
+        legend=True,
+        vmin=0,
+        vmax=1,
+        ax=ax
+    )
+
+    ax.set_title("Rezultati po točkah (%)")
+    ax.axis("off")
+
+    draw_districts(ax)
+    draw_units(ax)
+
+    plt.show()
+
+
+
+
+draw_map("DEMOKRATI-%", True, False)
+# draw_dots()
+# posrafiraj()
+
+
+
+
+
---
+++ AnalizaVolitev/main.py
@@ -0,0 +1,35 @@
+from pyproj import Transformer
+import json
+
+from izidi_zemljevid import *
+
+def to_geojson():
+
+    transformer = Transformer.from_crs("EPSG:3794", "EPSG:4326", always_xy=True)
+
+    with open("sredisca_1.json") as f:
+        data = json.load(f)
+
+    features = []
+
+    for name, x, y in data:
+        lon, lat = transformer.transform(x, y)
+
+        features.append({
+            "type": "Feature",
+            "properties": {"name": name},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [lon, lat]  # <-- ZDAJ PRAVILNO
+            }
+        })
+
+    geojson = {"type": "FeatureCollection", "features": features}
+
+    with open("tocke.geojson", "w") as f:
+        json.dump(geojson, f, indent=2, ensure_ascii=False)
+
+
+# draw_map("SVOBODA- odstotek", False)
+draw_dots()
+
